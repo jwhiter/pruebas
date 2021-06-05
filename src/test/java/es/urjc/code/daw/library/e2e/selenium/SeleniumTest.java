a938010319b806982543dcb8dc596ac4101b76fa
ab <a.elismaili.2017@alumnos.urjc.es>
2021-06-05 17:35:04 +0200

    añadir import de chromeOptions

--- src/test/java/es/urjc/code/daw/library/e2e/selenium/SeleniumTest.java
+++ src/test/java/es/urjc/code/daw/library/e2e/selenium/SeleniumTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
